eb4134755fa5136cca13ae6bf64346192c77bc6f
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-22 23:52:32 +0900

    Headbarコンポーネント作成、Webフォント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.scss";
+import Headbar from "./components/headbar/Headbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>aaaaaaaaaaa</h1>
+      <Headbar />
     </div>
   );
 }
---
+++ src/components/headbar/Headbar.tsx
@@ -0,0 +1,11 @@
+import "./Headbar.scss";
+const Headbar = () => {
+  return (
+    <div className="headbar">
+      <h1 className="headbar__title">Dairy Timeline App</h1>
+      <div className="headbar__inputArea"></div>
+    </div>
+  );
+};
+
+export default Headbar;
