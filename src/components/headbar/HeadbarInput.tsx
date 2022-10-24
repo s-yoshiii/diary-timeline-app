3616af07e92dd4b956da94c25b01cbbcfec2b4e2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-24 23:59:06 +0900

    inputエリア調整、mixin追加、変数追加

--- src/components/headbar/HeadbarInput.tsx
+++ src/components/headbar/HeadbarInput.tsx
@@ -1,7 +1,11 @@
 const HeadbarInput = () => {
   return (
     <div className="headbarInput">
-      <div className="headbarInput__inner">HeadbarInput</div>
+      <div className="headbarInput__inner">
+        <form action="">
+          <input type="text" placeholder="How did you do today?" />
+        </form>
+      </div>
     </div>
   );
 };
