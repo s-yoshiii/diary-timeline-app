4b8e6807ccd96443acb6a3ce7973198f63aac5d2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-26 00:49:23 +0900

    ドラッグ&ドロップでファイルをアップロードできるコンポーネント利用

--- src/components/headbar/HeadbarInput.tsx
+++ src/components/headbar/HeadbarInput.tsx
@@ -1,9 +1,18 @@
+import { useDropzone } from "react-dropzone";
+
 const HeadbarInput = () => {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const files = acceptedFiles.map((file) => <li>{file.path}</li>);
   return (
     <div className="headbarInput">
       <div className="headbarInput__inner">
         <form action="">
           <input type="text" placeholder="How did you do today?" />
+          <div {...getRootProps({ className: "dropzone" })}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+          <ul>{files}</ul>
         </form>
       </div>
     </div>
