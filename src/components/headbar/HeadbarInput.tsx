239fb528fa6f81a034b4824543c5d98e9e4e5d68
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-28 00:40:13 +0900

    アップロードモジュール調整

--- src/components/headbar/HeadbarInput.tsx
+++ src/components/headbar/HeadbarInput.tsx
@@ -1,5 +1,5 @@
 import { useDropzone } from "react-dropzone";
-
+import "css.gg/icons/scss/software-upload.scss";
 const HeadbarInput = () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
   const files = acceptedFiles.map((file) => <li>{file.path}</li>);
@@ -8,9 +8,12 @@ const HeadbarInput = () => {
       <div className="headbarInput__inner">
         <form action="">
           <input type="text" placeholder="How did you do today?" />
-          <div {...getRootProps({ className: "dropzone" })}>
+          <div {...getRootProps({ className: "inputfile" })}>
             <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <p>
+              <i className="gg-software-upload"></i>Drag 'n' drop some files
+              here, or click to select files
+            </p>
           </div>
           <ul>{files}</ul>
         </form>
