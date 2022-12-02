72b0bf58daa35def277caaf9715d1ccf4cd78b1a
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-02 12:55:58 +0900

    タイムライン時系列調整

--- src/components/timeline/TimelineItem.tsx
+++ src/components/timeline/TimelineItem.tsx
@@ -3,6 +3,9 @@ import React, { FC } from "react";
 const TimelineItem: FC = () => {
   return (
     <div className="timeline__item">
+      <time className="timeline__date" dateTime="2018-07-07">
+        2018/07/07
+      </time>
       <div className="timeline__itemInner">
         <div className="timeline__photo">
           <img src="https://source.unsplash.com/random" alt="" />
